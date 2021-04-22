339cd280037ab1959f7cea0ad9fb36913f0fea77
Documenter.jl <documenter@juliadocs.github.io>
2021-04-22 08:08:05 +0000

    build based on 866a2fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlasticNetwork","category":"page"},{"location":"#PlasticNetwork","page":"Home","title":"PlasticNetwork","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlasticNetwork.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlasticNetwork]","category":"page"}]
+}
